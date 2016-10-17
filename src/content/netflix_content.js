c2b427a32664b84c742bb9a3d5ba0c78c0835e92
Marcos Ferreira <merkkp@gmail.com>
2016-10-17 16:22:39 -0300

    add injection of content script in netlflix page
    doing the manifest way cuz i couldnt do it the right way

---
+++ src/content/netflix_content.js
@@ -0,0 +1,17 @@
+(function(){
+  'use strict';
+
+  var elements = {
+    showInfo: {
+      class: 'player-status'
+    },
+
+    progress: {
+      class: 'player-something',
+      styleElement: 'widht-or-someshit'
+    }
+
+  };
+
+  console.log('contet-stciprt');
+})();
